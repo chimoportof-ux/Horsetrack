7cc704cdbd946f40882f0c6af3f2a1dc16066b1f
David <a25davidpf@iessanclemente.net>
2026-03-27 11:55:57 +0100

    Configurar PostgreSQL en Render

--- horsetrack_project/settings.py
+++ horsetrack_project/settings.py
@@ -1,9 +1,10 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-dev-key')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key')
 DEBUG = "False"
 ALLOWED_HOSTS = ["*"]
 
@@ -48,10 +49,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'horsetrack_project.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
